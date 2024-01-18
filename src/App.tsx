7b66c53cb24c8045c5cc2963ec5d1a603fbd9d10
FudgyDRS <yetimonsters@gmail.com>
2024-01-18 09:53:16 +0100

    added family

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { WagmiConfig, createConfig } from 'wagmi';
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from 'connectkit';
 
-function App() {
+interface AppProps {}
+
+const config = createConfig(
+  getDefaultConfig({
+    alchemyId: process.env.ALCHEMY_ID,
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID!,
+
+    appName: 'FudgyDRS LFGHO',
+
+    appDescription: 'Project for LFGHO hackathon',
+    appUrl: 'https://github.com/FudgyDRS/lfgho-hackathon-frontend',
+    appIcon: 'https://family.co/logo.png',
+  }),
+);
+
+const App: React.FC<AppProps> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        {/* Your App */}
+        <ConnectKitButton />
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
-}
+};
 
 export default App;
